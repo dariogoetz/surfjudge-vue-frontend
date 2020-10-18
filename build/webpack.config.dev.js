516ad2e5ff7e4f8672fc778fc598f0ce42e942f3
Dario Götz <dario.goetz@googlemail.com>
2020-10-18 21:44:50 +0200

    [mod] add babel; add working css, stylus; hot reloading

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,21 +1,58 @@
 'use strict'
 
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: [
-        './src/index.js'
-    ],
+    // index.js is standard, so setting the entry is not required
+    // entry: [
+    //     './src/index.js'
+    // ],
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
     module: {
         rules: [
             {
                 test: /\.vue$/,
                 use: 'vue-loader'
+            }, {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }, {
+                test: /\.styl(us)?$/,
+                use: [
+                    'vue-style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            }, {
+                test: /\.js$/,
+                use: 'babel-loader'
             }
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
     ]
 }
