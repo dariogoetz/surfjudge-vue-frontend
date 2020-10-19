58ca2e12567fbfbb990331dd157f91aa6a0c6d02
Dario Götz <dario.goetz@googlemail.com>
2020-10-19 22:33:28 +0200

    [mod] working dropdown menus in results view

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,9 +1,80 @@
 <template>
-  <h1>Hello Dropdown!</h1>
+  <div>
+    <button
+      class="btn btn-secondary dropdown-toggle"
+      type="button"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      {{ label }}
+    </button>
+    <div class="dropdown-menu">
+      <a
+        v-for="elem in elems"
+        :key="elem.id"
+        class="dropdown-item"
+        href="#"
+        @click="selected(elem)"
+      >
+        {{ elem.name }}
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    url: {
+      type: String,
+      default: null
+    },
+    defaultLabel: {
+      type: String,
+      default: 'Select'
+    },
+    labelKey: {
+      type: String,
+      default: 'name'
+    }
+  },
+  data () {
+    return {
+      elems: [],
+      current_elem: null
+    }
+  },
+  computed: {
+    label: function () { return this.current_elem === null ? this.defaultLabel : this.current_elem[this.labelKey] }
+  },
+  watch: {
+    url () {
+      this.refresh()
+    }
+  },
+  created () {
+    this.refresh()
+  },
+  methods: {
+    refresh () {
+      if (this.url === null) {
+        return
+      }
+      console.log('Fetching from', this.url)
+      fetch(this.url)
+        .then(response => response.json())
+        .then(data => {
+          this.elems = data
+          console.log('Dropdown menu fetched', data)
+        })
+    },
+    selected (elem) {
+      console.log('Dropdown menu: selected', elem)
+      this.current_elem = elem
+      this.$emit('selected', elem)
+    }
+  }
 }
 </script>
 
--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <dropdown-menu url="/rest/tournaments" />
-    <dropdown-menu url="/rest/categories" />
+    <dropdown-menu
+      :url="tournament_dropdown_url"
+      default-label="Select Tournament"
+      @selected="select_tournament"
+    />
+    <dropdown-menu
+      :url="category_dropdown_url"
+      default-label="Select Category"
+      @selected="select_category"
+    />
     <result-table />
   </div>
 </template>
@@ -14,6 +22,28 @@ export default {
   components: {
     DropdownMenu,
     ResultTable
+  },
+  data () {
+    return {
+      tournament_id: null
+    }
+  },
+  computed: {
+    tournament_dropdown_url () {
+      return 'http://localhost:8081/rest/tournaments'
+    },
+    category_dropdown_url () {
+      return this.tournament_id === null ? null : `http://localhost:8081/rest/tournaments/${this.tournament_id}/categories`
+    }
+  },
+  methods: {
+    select_tournament (tournament) {
+      console.log('Results: selected', tournament)
+      this.tournament_id = tournament.id
+    },
+    select_category (category) {
+      console.log('Results: selected', category)
+    }
   }
 }
 </script>
