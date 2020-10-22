6894cf9f672e619460ee499e28f1d593167d00f0
Dario Götz <dario.goetz@googlemail.com>
2020-10-22 15:08:37 +0200

    [mod] load vue routes lazily

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../pages/Index.vue'
-import Results from '../pages/Results.vue'
-import LiveResults from '../pages/LiveResults.vue'
-import Heatcharts from '../pages/Heatcharts.vue'
+
+// import routes lazily
+const Results = () => import('../pages/Results.vue')
+const LiveResults = () => import('../pages/LiveResults.vue')
+const Heatcharts = () => import('../pages/Heatcharts.vue')
 
 Vue.use(VueRouter)
 
