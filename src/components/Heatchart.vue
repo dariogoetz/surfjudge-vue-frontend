9f4538c97826b676ae1648dc63119ee7a5affbd6
Dario Götz <dario.goetz@googlemail.com>
2020-11-02 21:26:40 +0100

    [add] first dummy heatchart component and page

---
+++ src/components/Heatchart.vue
@@ -0,0 +1,239 @@
+<template>
+  <div id="heatchart" />
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+  props: {
+    category: { type: Object, default: null },
+
+    showIndividualScores: { type: Boolean, default: false },
+    showTotalScores: { type: Boolean, default: false },
+
+    targetWidth: { type: Number, default: null },
+    scalingFactor: { type: Number, default: 1.25 },
+    marginTop: { type: Number, default: 0 },
+    marginBottom: { type: Number, default: 0 },
+    marginLeft: { type: Number, default: 0 },
+    marginRight: { type: Number, default: 0 },
+
+    heatWidth: { type: Number, default: 280 },
+    heatVerticalSpacing: { type: Number, default: 50 },
+    heatHorizontalSpacing: { type: Number, default: 100 },
+    rowHeight: { type: Number, default: 18 }
+
+  },
+  data () {
+    return {
+      heats: null, // array
+      advancements: null, // array
+      participations: null, // array
+      results: null
+    }
+  },
+  computed: {
+    width () {
+      if (this.targetWidth === null) return this.scalingFactor * this.internalWidth
+      else return Math.floor(Math.min(this.targetWidth, this.scalingFactor * this.internalWidth)) - 5
+    },
+    internalWidth () { return 100 },
+    internalHeight () { return 100 },
+    viewBox () {
+      console.debug('compute viewBox')
+      const ext2int = this.internalWidth / (this.width - this.marginLeft - this.marginRight)
+      const ul = -this.marginLeft * ext2int
+      const ur = -this.marginRight * ext2int
+      const ll = this.internalWidth + (this.marginLeft + this.marginRight) * ext2int
+      const lr = this.internalHeight + (this.marginTop + this.marginBottom) * ext2int
+      return `${ul} ${ur} ${ll} ${lr}`
+    },
+    heatsMap () {
+      console.debug('compute heatsMap')
+      if (this.heats === null) return new Map()
+      const res = new Map()
+      this.heats.forEach((heat) => res.set(heat.id, heat))
+      return res
+    },
+    resultsMap () {
+      console.debug('compute resultsMap')
+      if (this.results === null) return new Map()
+      const res = new Map()
+      this.results.forEach((r) => {
+        if (!res.has(r.heat_id)) res.set(r.heat_id, [])
+        res.get(r.heat_id).push(r)
+      })
+      res.forEach((rs) => rs.sort(
+        (a, b) => a.place - b.place
+      ))
+      return res
+    },
+    participationsMap () {
+      console.debug('compute participationsMap')
+      if (this.participations === null) return new Map()
+      const res = new Map()
+      this.participations.forEach((part) => {
+        if (!res.has(part.heat_id)) res.set(part.heat_id, [])
+        res.get(part.heat_id).push(part)
+      })
+      res.forEach((parts) => parts.sort(
+        (a, b) => a.seed - b.seed
+      ))
+      return res
+    },
+    toAdvancementsMap () {
+      console.debug('compute toAdvancementsMap')
+      if (this.advancements === null) return new Map()
+      const res = new Map()
+      this.advancements.forEach((adv) => {
+        if (!res.has(adv.to_heat_id)) res.set(adv.to_heat_id, [])
+        res.get(adv.to_heat_id).push(adv)
+      })
+      res.forEach((advs) => advs.sort(
+        (a, b) => a.seed - b.seed
+      ))
+      return res
+    },
+    round2Heats () {
+      console.debug('compute round2Heats')
+      if (this.heats === null) return new Map()
+      const res = new Map()
+      this.heats.forEach((heat) => {
+        if (!res.has(heat.round)) res.set(heat.round, [])
+        res.get(heat.round).push(heat)
+      })
+      // sort heats in round
+      res.forEach(
+        (heats) => heats.sort(
+          (a, b) => a.number_in_round - b.number_in_round
+        )
+      )
+      return res
+    },
+    nParticipants () {
+      if (this.heats === null) return new Map()
+      if (this.participations === null) return new Map()
+      if (this.advancements === null) return new Map()
+
+      console.debug('compute nParticipants')
+      // map: heat_id -> max(max_seed_in_link, max_seed_participations)
+      const res = new Map()
+      this.heats.forEach((heat) => {
+        const advanced = (this.toAdvancementsMap.get(heat.id) || [])
+        const maxAdv = Math.max(
+          0,
+          ...advanced.map((adv) => adv.seed + 1)
+        )
+        const part = (this.participationsMap.get(heat.id) || [])
+        const maxPart = Math.max(
+          0,
+          ...part.map((p) => p.seed + 1)
+        )
+        const nParticipants = Math.max(
+          maxAdv,
+          maxPart
+        )
+        res.set(heat.id, nParticipants)
+      })
+      return res
+    },
+    round2Slots () {
+      console.debug('compute round2Slots')
+      // sum all numbers of participants for heats an each round
+      const res = new Map()
+      this.round2Heats.forEach((heats, round) => {
+        res.set(round,
+          heats
+            .map((h) => this.nParticipants.get(h.id) || 0)
+            .reduce((a, b) => a + b, 0)
+        )
+      })
+      // collect heat data required for coordinate computation
+      return res
+    },
+    heatCoordinates () {
+      console.debug('compute heatCoordinates')
+      const res = new Map()
+      Array.from(this.round2Heats.keys())
+        .sort()
+        .forEach((round, roundIdx) => {
+          let slots = 0
+          this.round2Heats.get(round)
+            .sort((a, b) => a.number_in_round - b.number_in_round)
+            .forEach((heat, idx) => {
+              res.set(heat.id, [roundIdx, slots])
+              slots += this.nParticipants.get(heat.id) || 0
+            })
+        })
+      return res
+    },
+    processedHeats () {
+      if (this.heats === null) return new Map()
+      console.debug('compute processedHeats')
+      // generate an map of heats consisting of
+      // id, nParticipants, round, numberInRound
+      const res = new Map()
+      this.heats.forEach((heat) => {
+        const procHeat = Object.assign({}, heat)
+        procHeat.participations = this.participationsMap.get(heat.id) || []
+        procHeat.results = this.resultsMap.get(heat.id) || []
+        procHeat.nParticipants = this.nParticipants.get(heat.id) || 0
+        procHeat.coordinatess = this.heatCoordinates.get(heat.id)
+        res.set(heat.id, procHeat)
+      })
+
+      return res
+    },
+    processedLinks () {},
+  },
+  mounted () {
+    this.initSvg()
+    Promise.all([
+      this.fetchHeats(),
+      this.fetchResults(),
+      this.fetchAdvancements(),
+      this.fetchParticipations()
+    ]).then(() => {
+      console.debug('fetched all')
+      console.log(this.processedHeats)
+    })
+  },
+  methods: {
+    initSvg () {
+      const svg = d3
+        .select('#heatchart')
+        .append('svg')
+        .attr('viewBox', this.viewBox)
+        .attr('preserveAspectRatio', 'xMinYMin meet')
+        .attr('width', this.width)
+        .attr('height', this.width * (this.internalHeight / this.internalWidth) || 0)
+        .attr('class', 'heatchart')
+    },
+    fetchHeats () {
+      return fetch('http://localhost:8081/rest/categories/1/heats')
+        .then(response => response.json())
+        .then(data => { this.heats = data })
+    },
+    fetchAdvancements () {
+      return fetch('http://localhost:8081/rest/categories/1/advancements')
+        .then(response => response.json())
+        .then(data => { this.advancements = data })
+    },
+    fetchResults () {
+      return fetch('http://localhost:8081/rest/categories/1/results')
+        .then(response => response.json())
+        .then(data => { this.results = data })
+    },
+    fetchParticipations () {
+      return fetch('http://localhost:8081/rest/categories/1/participations')
+        .then(response => response.json())
+        .then(data => { this.participations = data })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/pages/Heatcharts.vue
+++ src/pages/Heatcharts.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <span>Heatcharts</span>
+    <heatchart />
   </div>
 </template>
 
 <script>
+import Heatchart from '../components/Heatchart.vue'
 
 export default {
+  components: {
+    Heatchart
+  },
   props: {
     tournament: {
       type: Object,
