2745bbd97e3099832c18321ec2fb11e88094e549
Dario Götz <dario.goetz@googlemail.com>
2021-08-08 21:35:54 +0200

    [mod] slot for action prepared

--- src/components/JudgeRequests.vue
+++ src/components/JudgeRequests.vue
@@ -5,18 +5,14 @@
       :fields="fields"
       :tbody-tr-class="row_class"
       class="table judging_requests_table"
-      data-toggle="table"
-      data-sort-name="judge_id"
-      date-sort-order="asc">
-      <thead>
-        <tr>
-          <th data-field="judge_id">Judge ID</th>
-          <th data-field="name" data-sortable="true">Judge Name</th>
-          <th data-field="expires" data-sortable="true">Expires</th>
-          <th data-field="status" data-sortable="true">Status</th>
-          <th data-field="action">Action</th>
-        </tr>
-      </thead>
+      borderless
+      no-select-on-click
+    >
+      
+      <template #cell(action)="data">
+        <div v-if="data.item.status==='confirmed'">Hallo</div>
+      </template>
+
     </b-table>
     <b-button variant="secondary" class="add_judges_btn"><b-icon-plus />&nbsp;Add Judges</b-button>
   </div>
